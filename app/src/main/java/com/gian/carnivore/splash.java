fd131e0a0d097a618cf02268c810f04f89eedba7
1Gian <giansemi@gmail.com>
2015-11-02 01:56:21 +0300

    splash layout

--- app/src/main/java/com/gian/carnivore/splash.java
+++ app/src/main/java/com/gian/carnivore/splash.java
@@ -12,6 +12,7 @@ public class splash extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
     }
 
 }
