c97c46436542c50ee8e7a2894ea0af64379d65f3
1Gian <giansemi@gmail.com>
2015-11-02 01:44:46 +0300

    splash

---
+++ app/src/main/java/com/gian/carnivore/splash.java
@@ -0,0 +1,17 @@
+package com.gian.carnivore;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gian on 11/2/15.
+ */
+public class splash extends Activity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
